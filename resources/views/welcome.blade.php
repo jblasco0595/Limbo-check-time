e46d5a6fc3bd277ec3b35816d8e65eef19a651aa
Jorge Blasco <jorgeblasco0595@gmail.com>
2020-10-06 16:45:41 -0300

    changes in welcome done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Limbocheck</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LimboCheck
                 </div>
 
                 <div class="links">
