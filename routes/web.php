4e7e0da78d6d5fcc4818d7eee5af0c40195693c3
Jorge Blasco <jorgeblasco0595@gmail.com>
2020-11-03 12:40:52 -0300

    web and .env change for proxy L

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
+
+
+
+$proxy_url    = getenv('PROXY_URL');
+$proxy_schema = getenv('PROXY_SCHEMA');
+
+if (!empty($proxy_url)) {
+   URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+    URL::forceScheme($proxy_schema);
+}
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
